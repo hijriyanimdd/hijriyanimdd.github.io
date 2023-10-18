a408c252e386decbedbbded2c581784d5d385999
Hijriyani Mardhotillah <91871452+hijriyanimdd@users.noreply.github.com>
2023-10-18 11:30:24 +0700

    Add files via upload

---
+++ Klasifikasi KNN.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Laporan Praktikum KNN
+
+# In[2]:
+
+
+#import loat package
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[6]:
+
+
+#menampilkan dataset dari Immunotherapy
+immunotherapy = pd.read_xlsx("Immunotherapy.xlsx",
+delimiter = ",")
+
+
+# In[ ]:
+
+
+
+
